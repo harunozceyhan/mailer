08f2953954c06b6e248ec73627a95092d7462045
Harun Ozceyhan <harunozceyhan@gmail.com>
2020-02-17 10:40:47 +0300

    feat: test libraries added feat: mail controller unit test aded

---
+++ src/test/java/com/ozceyhan/mailer/controller/MailControllerTest.java
@@ -0,0 +1,38 @@
+package com.ozceyhan.mailer.controller;
+
+import com.ozceyhan.mailer.model.Mail;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.ozceyhan.mailer.controller.MailController;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Vinoth.Gopu Test class for mail controller
+ */
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class MailControllerTest {
+
+    @Mock
+    private MailController mailController;
+
+    /**
+     * Test method for
+     * {@link com.ozceyhan.mailer.controller.MailController#send(com.ozceyhan.mailer.model.Mail)}.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void sendMailTest() throws Exception {
+        Mockito.when(mailController.sendMail(Mockito.any(Mail.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+        ResponseEntity<?> response = mailController.sendMail(new Mail());
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+}
