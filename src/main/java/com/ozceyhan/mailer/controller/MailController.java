a025c056e7d1371b9b13960b77746efd0fc1fa26
Harun Özceyhan <harunozceyhan@smartiys.com>
2020-02-17 03:20:57 +0300

    feat: swagger ui added

--- src/main/java/com/ozceyhan/mailer/controller/MailController.java
+++ src/main/java/com/ozceyhan/mailer/controller/MailController.java
@@ -1,5 +1,7 @@
 package com.ozceyhan.mailer.controller;
 
+import javax.validation.Valid;
+
 import com.ozceyhan.mailer.model.Mail;
 import com.ozceyhan.mailer.producer.MailProducer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +12,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/mail")
 public class MailController {
 
     @Autowired
     private MailProducer mailProducer;
 
-    @PostMapping(value = "/", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> sendMail(@RequestBody Mail mail) {
+    @PostMapping(value = "", consumes = "application/json", produces = "application/json")
+    @Operation(description = "Sends Mail to Kafka Producer", responses = {
+            @ApiResponse(responseCode = "200", description = "Mail Sent to Kafka Producer") })
+    public ResponseEntity<?> sendMail(@Valid @RequestBody Mail mail) {
         mailProducer.sendMessage(mail);
         return new ResponseEntity<>(HttpStatus.OK);
     }
--- src/main/java/com/ozceyhan/mailer/model/Mail.java
+++ src/main/java/com/ozceyhan/mailer/model/Mail.java
@@ -1,5 +1,8 @@
 package com.ozceyhan.mailer.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Mail {
+
+    @NotNull
+    @Email
     String to;
+    @NotNull
     String subject;
+    @NotNull
     String text;
+
     String attachmentUri;
 }
